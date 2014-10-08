ceec8e3163eb60c9d07ecb7f7c2dfbbc95f78ab6
Francesc Elies Henar <felies@visualfabriq.com>
2014-10-08 10:39:49 +0200

    clean up

--- setup.py
+++ setup.py
@@ -1,21 +1,10 @@
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
+
 import numpy as np
 
-setup(
-    name='khash_test',
-    version='0.0.1',
-    packages=[''],
-    url='',
-    license='',
-    author='Carst Vaartjes',
-    author_email='carstvaartjes@gmail.com',
-    description='A try out for a pandas-independent implementation of klib in Python'
-)
-
 inc_dirs = [
-    # 'khash',
     'khash/src/klib',
     'khash/src',
     np.get_include(),
@@ -24,6 +13,7 @@ inc_dirs = [
 lib_dirs = []
 libs = []
 def_macros = []
+
 sources = [
     "khash/hashtable.pyx",
     "khash/hashtable.pxd"
@@ -45,11 +35,19 @@ ext_modules = [
 cmdclass = {'build_ext': build_ext}
 
 setup(
-    name='vf_api',
+    name='khash',
+    version='0.0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='Carst Vaartjes',
+    author_email='carstvaartjes@gmail.com',
+    description='''A try out for a pandas-independent
+        implementation of klib in Python''',
+    # -----
     cmdclass=cmdclass,
-    # include_dirs = inc_dirs,
     ext_modules=ext_modules,
-    requires=[]
+    requires=[],
 )
 
 # Compiled with:
